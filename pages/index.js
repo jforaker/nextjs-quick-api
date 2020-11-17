2a3682b2cc023ee9001eb41b2c4b163702d25684
Jake Foraker <jakeforaker83@gmail.com>
2020-11-17 00:26:16 -0500

    move theme to styles dir

--- pages/index.js
+++ pages/index.js
@@ -11,43 +11,7 @@ import styled from '@emotion/styled';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import FormShell from '../components/FormShell';
-
-const ACCENT = '#64ffda';
-const DARK_BG = '#020c1b';
-const BG = '#0a192f';
-
-const theme = {
-  colors: {
-    darkNavy: DARK_BG,
-    navy: BG,
-    lightNavy: '#172a45',
-    lightestNavy: '#303C55',
-    slate: '#8892b0',
-    lightSlate: '#a8b2d1',
-    lightestSlate: '#ccd6f6',
-    white: '#e6f1ff',
-    green: ACCENT,
-    greenTint: 'rgba(100, 255, 218, 0.07)',
-    greenTransparent: '#64ffd5ad',
-    pink: '#b61aae',
-  },
-
-  fonts: {
-    Calibre:
-      'Calibre, San Francisco, SF Pro Text, -apple-system, system-ui, BlinkMacSystemFont, Roboto, Helvetica Neue, Segoe UI, Arial, sans-serif',
-    SFMono:
-      'SF Mono, Fira Code, Fira Mono, Roboto Mono, Lucida Console, Monaco, monospace',
-  },
-
-  fontSizes: [14, 16, 18, 20, 24],
-
-  easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
-  transition: 'all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1)',
-
-  borderRadius: '4px',
-  margin: '20px',
-  radius: 4,
-};
+import theme from '../styles/theme';
 
 const HomeContainer = styled.main`
   min-height: 100vh;
---
+++ styles/theme.js
@@ -0,0 +1,32 @@
+export default {
+  colors: {
+    darkNavy: '#020c1b',
+    navy: '#0a192f',
+    lightNavy: '#172a45',
+    lightestNavy: '#303C55',
+    slate: '#8892b0',
+    lightSlate: '#a8b2d1',
+    lightestSlate: '#ccd6f6',
+    white: '#e6f1ff',
+    green: '#64ffda',
+    greenTint: 'rgba(100, 255, 218, 0.07)',
+    greenTransparent: '#64ffd5ad',
+    pink: '#b61aae',
+  },
+
+  fonts: {
+    Calibre:
+      'Calibre, San Francisco, SF Pro Text, -apple-system, system-ui, BlinkMacSystemFont, Roboto, Helvetica Neue, Segoe UI, Arial, sans-serif',
+    SFMono:
+      'SF Mono, Fira Code, Fira Mono, Roboto Mono, Lucida Console, Monaco, monospace',
+  },
+
+  fontSizes: [14, 16, 18, 20, 24],
+
+  easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+  transition: 'all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1)',
+
+  borderRadius: '4px',
+  margin: '20px',
+  radius: 4,
+};
